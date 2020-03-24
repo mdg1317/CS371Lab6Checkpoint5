185e606a8f07121ddf3f097bc83e0adb7624d126
mdg1317 <matthew.dennis137@gmail.com>
2020-03-23 22:04:10 -0700

    Added TTTStateTest

---
+++ app/src/test/java/edu/up/cs301/tictactoe/TTTStateTest.java
@@ -0,0 +1,27 @@
+package edu.up.cs301.tictactoe;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TTTStateTest {
+
+    @Test
+    public void getPiece() {
+        TTTState testState = new TTTState();
+        char place = testState.getPiece(-1,0);
+        assertEquals('?', place);
+    }
+
+    @Test
+    public void setPiece() {
+    }
+
+    @Test
+    public void getWhoseMove() {
+    }
+
+    @Test
+    public void setWhoseMove() {
+    }
+}
