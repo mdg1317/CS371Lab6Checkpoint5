09dd12c5d9ac29b00649e3d994e026e654854056
William Lau <iamwill8888@yahoo.com>
2020-03-23 22:26:07 -0700

    checkpoint 5 will getWhoseMove

--- app/src/test/java/edu/up/cs301/tictactoe/TTTStateTest.java
+++ app/src/test/java/edu/up/cs301/tictactoe/TTTStateTest.java
@@ -15,10 +15,16 @@ public class TTTStateTest {
 
     @Test
     public void setPiece() {
+
     }
 
     @Test
     public void getWhoseMove() {
+        TTTState testState = new TTTState();
+        testState.setWhoseMove(0);
+        int move = testState.getWhoseMove();
+        assertEquals(move, 0);
+
     }
 
     @Test
